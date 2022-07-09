aa20fadef4ab244b10abfb93325a0448321cccf2
mrfor <truevladimir93Dev@yandex.ru>
2022-07-09 21:58:11 +0300

    refact Archivator

--- first_task/src/main/java/com/example/first_task/first_task/Archivator.java
+++ first_task/src/main/java/com/example/first_task/first_task/Archivator.java
@@ -5,40 +5,38 @@ import java.util.stream.Collectors;
 
 public class Archivator {
 
-    public String doArchiveStr(String str) {
-        if (str.isEmpty()) {
+    public String doArchiveStr(String inputString) {
+        if (inputString.isEmpty()) {
             return "Недопустимое значение: Введенная строка пустая";
         }
 
-        //проверка на наличиче чисел или пробелов
-        if (str.matches(".*[0-9].*|\\s*")) {
+        if (inputString.matches(".*[0-9].*|\\s*")) {
             return "недопустимое значение: Строка содержит цифры или пробел";
         }
 
-        StringBuilder sb = new StringBuilder();
-        //лист значений из получаемой строки
-        List<Character> strChars = str.chars().mapToObj(ch -> (char) ch).collect(Collectors.toList());
+        StringBuilder stringBuilder = new StringBuilder();
+        List<Character> charsFromInputString = inputString.chars().mapToObj(ch -> (char) ch).collect(Collectors.toList());
 
-
-        //
-        Character bufCh;//текущее значение для цикла
-        int cnt = 0; //счётчик
-        for (int i = 0; i < strChars.size(); i++) {
-            cnt++;
-            bufCh = strChars.get(i);
-            if ((i + 1) < strChars.size() && bufCh == strChars.get(i + 1)) { //если есть следующий элемент и если он раввен текущему
+        Character currentChar;
+        int count = 0;
+        for (int i = 0; i < charsFromInputString.size(); i++) {
+            count++;
+            currentChar = charsFromInputString.get(i);
+            if ((i + 1) < charsFromInputString.size() && currentChar == charsFromInputString.get(i + 1)) {
+                //если есть следующий элемент и если он раввен текущему
                 continue;
-            } else if (cnt == 1) { //если счетчик 1, добавляем просто символ, обнуляем cnt
-                sb.append(bufCh);
-                cnt = 0;
-            } else { //иначе добавляем количество текущего символа и сам символ, обнуляем cnt
-                sb.append(cnt);
-                sb.append(bufCh);
-                cnt = 0;
+            } else if (count == 1) {
+                //если счетчик 1, добавляем просто символ, обнуляем count
+                stringBuilder.append(currentChar);
+                count = 0;
+            } else {
+                //иначе добавляем количество текущего символа и сам символ, обнуляем count
+                stringBuilder.append(count);
+                stringBuilder.append(currentChar);
+                count = 0;
             }
         }
-
-        return sb.toString();
+        return stringBuilder.toString();
     }
 
 }
